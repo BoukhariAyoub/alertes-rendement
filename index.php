5a149528fe726ca1c8e291e949230115d01522a5
Ayoub Boukhari <Boukhari.ayoub14@gmail.com>
2018-01-09 11:24:12 +0100

    add le bon coin library

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'vendor/autoload.php';
+include 'lib/leboncoin.php';
 
 function ServiceHandler() {
     $data = new stdClass();
@@ -9,8 +10,30 @@ function ServiceHandler() {
     return json_encode($data);
 };
 
+function getAnnonces(){
+$leboncoin = new LeBonCoin();
+$options = array(
+		"search_title_only" => 1,
+		"localisation" => array("34500","34000","91000"),
+		"categorie" => $leboncoin->searchCategorie("informatique")->code,
+		"prix_min" => 150,
+		"prix_max" => 10000,
+		"particulier" => true,
+		"pro" => false,
+		"urgent_only" => false
+	);
+
+	$annonces = $leboncoin->getAnnonces("AlienWare", 1, $options);
+
+    return json_encode($annonces);
+};
+
 $app = new \Slim\App;
 
+
+	
+	
+$app->get('/Annonces', 'getAnnonces');
 $app->get('/', 'ServiceHandler');
 $app->post('/', 'ServiceHandler');
 
